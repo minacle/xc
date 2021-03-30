c30bfbcd3bc311e6a30bed1ca041d3f9beb4e044
Mina Her <minacle@live.com>
2021-03-31 01:20:20 +0900

    Add test case to XcKitTests
    - CustomStringConvertibleTests

---
+++ Tests/XcKitTests/CustomStringConvertibleTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+@testable
+import XcKit
+
+final class CustomStringConvertibleTests: XCTestCase {
+
+    func testBuild() {
+        XCTAssertEqual(Xcode.Build().description, "1A1")
+        XCTAssertEqual(Xcode.Build(2, .a, 30, .f).description, "2A30f")
+        XCTAssertEqual(Xcode.Build(3, .c, 500).debugDescription, "3C500")
+        XCTAssertEqual(Xcode.Build(4, .d, 5050, .g).debugDescription, "4D5050g")
+    }
+
+    func testVersion() {
+        XCTAssertEqual(Xcode.Version().description, "0.0")
+        XCTAssertEqual(Xcode.Version().debugDescription, "0.0")
+        XCTAssertEqual(Xcode.Version(3, 1, 0).description, "3.1")
+        XCTAssertEqual(Xcode.Version(4, 2, 1).description, "4.2.1")
+        XCTAssertEqual(Xcode.Version(5, 0, 0).debugDescription, "5.0.0")
+        XCTAssertEqual(Xcode.Version(6, 1).debugDescription, "6.1")
+    }
+}
