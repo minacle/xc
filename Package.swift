674160687667e7fb6b49cbbbd7aabfa0a5aa74c8
Mina Her <minacle@live.com>
2021-07-18 19:25:12 +0900

    Update a dependency
    - https://github.com/apple/swift-argument-parser .upToNextMinor(from: "0.4.3")

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            .upToNextMinor(from: "0.4.1")),
+            .upToNextMinor(from: "0.4.3")),
     ],
     targets: [
         .target(
